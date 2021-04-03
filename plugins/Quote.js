0db919ae1f935f8caf0e15076c825f5605ddc330
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-04 02:11:02 +0800

    Update Quote.js

--- plugins/Quote.js
+++ plugins/Quote.js
@@ -1,32 +1,26 @@
-let imageToBase64 = require('image-to-base64');
-let axios = require("axios");
-let handler = async(m, { conn, text }) => {
+let fetch = require('node-fetch')
+
+let handler = async(m, { conn, args, usedPrefix }) => {
   await m.reply('*[ WAIT ]* _Sedang Diproses..._')
     let str = `
 *[ • SGDC-BOT • ]*
 `.trim()
     
-    axios.get(`https://raw.githubusercontent.com/Afdhan/Quo/main/file.txt`)
-      .then((result) => {
-        let b = JSON.parse(JSON.stringify(result.data));
-        let dhan =  b[Math.floor(Math.random() * b.length)];
-        imageToBase64(dhan)
-        .then(
-            (response) => {
-	let buf = Buffer.from(response, 'base64');
-
-    conn.sendFile(m.chat, buf, 'QUOTES-MUSLIM.png', str, m)
-            }
-        )
-        .catch(
-            (error) => {
-                console.log(error); 
-            }
-        )
-    
-    });
+            fetch('https://raw.githubusercontent.com/Afdhan/Quo/main/file.txt')
+            .then(res => res.text())
+            .then(body => {
+                let quo = body.split('\n')
+                let islami = quo[Math.floor(Math.random() * quo.length)]
+                conn.sendFile(m.chat, islami, '', 'Dasar Wibu:v', m)
+            })
+            .catch(() => {
+                conn.reply(m.chat, '*[ ERROR ]* _Terjadi Kesalahan!_', m)
+            })
+    } /*else {
+        conn.reply(m.chat, `Query tidak tersedia!. Silahkan ketik *${usedPrefix}anime* untuk melihat *List Query*`, m)
+    }*/
+//   } else if (!user.prems) m.reply('*FITUR INI KHUSUS UNTUK USER PREMIUM!*')
 }
-
 handler.command = /^(quotepict)$/i
 handler.owner = false
 handler.mods = false
