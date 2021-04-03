882bdc9cb8776802ca6eb8c130d48ac95ca97012
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-03 23:50:47 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install libwebp -y
+pkg install imagemagick -y
+npm install
+
+echo "[ JALANKAN SCRIPT DENGAN MENGETIK npm start / node index.js ]"
+echo "[ JIKA ERROR SILAHKAN KETIK node dhans.js / node server.js ]"
