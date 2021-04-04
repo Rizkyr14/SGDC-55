c57ab22f50b1bc90539130b90f69b59d3b1b78a9
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-04 17:34:36 +0800

    Update surah.js

--- plugins/surah.js
+++ plugins/surah.js
@@ -3,24 +3,12 @@ let handler = async(m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, '_Masukkan Nomor Surah_', m)
   await m.reply('*[ WAIT ]* _Searching..._')
 	axios.get(`https://api.zeks.xyz/api/quran?no=${text}&apikey=apivinz`).then ((res) => {
-	 	let hasil = `
-*HASIL SEARCHING*
-	
-*Nomor:* ${res.data.no}
-*Surah:* ${res.data.surah}
-*Asma:* ${res.data.asma}
-*Tipe:* ${res.data.type}
-*Jumlah Ayat:* ${res.data.jumlah_ayat}
-*Keterangan:*
-${res.data.ket}
+	 	let hasil = `*HASIL SEARCHING*\n\n*Nomor:* ${res.data.no}\n*Surah:* ${res.data.surah}\n*Asma:* ${res.data.asma}\n*Tipe:* ${res.data.type}
+*Jumlah Ayat:* ${res.data.jumlah_ayat}\n*Keterangan:*\n${res.data.ket}\n\n*Audio:*\n${res.data.audio}\n\n_Klik Link Diatas Jika Ingin Mendengarkan Surah ${res.data.surah}_\n`
+             for (let i = 0; i < res.data.ayat.length; i++) {
+                    hasil += `\n─────────────────\n\n• *Ayat:* ${res.data.ayat[i].number}\n${res.data.ayat[i].text}\n\n• *Artinya:* ${res.data.ayat[i].translation_id}\n`
+              }
 
-*Audio:*
-${res.data.audio}
-
-_Klik Link Diatas Jika Ingin Mendengarkan Surah ${res.data.surah}_
-
-*[ • SGDC-BOT • ]*
-`.trim()
 
     conn.reply(m.chat, hasil, m)
 	})
