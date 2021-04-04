a166669261f72f36813ef1182fd4cfef7604e9e6
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-04 14:32:41 +0800

    Kon

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -3,12 +3,12 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
 let chat = global.DATABASE.data.chats[m.chat]
 if (chat.simi) {
-// axios.get(`https://fzn-gaz.herokuapp.com/api/simi?text=${text}`).then((res) => {
- // let hh = `${res.data.result}`
-//conn.reply(m.chat, `${res.data.result}`, m)
-  let simi = pickRandom(global.simi)                         
-  conn.reply(m.chat, `${simi}`, m)
-  //}) 
+ axios.get(`https://fzn-gaz.herokuapp.com/api/simi?text=${text}`).then((res) => {
+  //let hh = `${res.data.result}`
+conn.reply(m.chat, `${res.data.result}`, m)
+ // let simi = pickRandom(global.simi)                         
+  //conn.reply(m.chat, `${simi}`, m)
+  }) 
  } else if (!chat.simi) m.reply('*SIMI BELUM DIAKTIFKAN UNTUK CHAT INI!*')
 }
                       
