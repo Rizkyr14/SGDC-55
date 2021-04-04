2ffce86ef8a28aeaa7a8ed1e5d9ecbc4570180f2
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-04 17:38:49 +0800

    Uu

--- plugins/surah.js
+++ plugins/surah.js
@@ -4,9 +4,9 @@ if (!text) return conn.reply(m.chat, '_Masukkan Nomor Surah_', m)
   await m.reply('*[ WAIT ]* _Searching..._')
 	axios.get(`https://api.zeks.xyz/api/quran?no=${text}&apikey=apivinz`).then ((res) => {
 	 	let hasil = `*HASIL SEARCHING*\n\n*Nomor:* ${res.data.no}\n*Surah:* ${res.data.surah}\n*Asma:* ${res.data.asma}\n*Tipe:* ${res.data.type}
-*Jumlah Ayat:* ${res.data.jumlah_ayat}\n*Keterangan:*\n${res.data.ket}\n\n*Audio:*\n${res.data.audio}\n\n_Klik Link Diatas Jika Ingin Mendengarkan Surah ${res.data.surah}_\n`
+*Jumlah Ayat:* ${res.data.jumlah_ayat}\n*Keterangan:*\n${res.data.ket}\n\n*Audio:*\n${res.data.audio}\n_Klik Link Diatas Jika Ingin Mendengarkan Surah ${res.data.surah}_\n`
              for (let i = 0; i < res.data.ayat.length; i++) {
-                    hasil += `\n─────────────────\n\n• *Ayat:* ${res.data.ayat[i].number}\n${res.data.ayat[i].text}\n\n• *Artinya:* ${res.data.ayat[i].translation_id}\n`
+                    hasil += `\n─────────────────\n\n${res.data.ayat[i].text}\n\n*Artinya:*\n_${res.data.ayat[i].translation_id}_\n`
               }
 
 
