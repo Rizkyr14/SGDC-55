fc32b2a3d47bd62349e9ca5c5dd6a0b860e21898
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-04 20:21:36 +0800

    Add files via upload

---
+++ plugins/tes.js
@@ -0,0 +1,18 @@
+let handler = async delete(m) => {
+    if (m.key.fromMe) return
+    let chat = global.DATABASE._data.chats[m.key.remoteJid]
+    if (chat.delete) return
+    await this.reply(m.key.remoteJid, `
+*_Terdeteksi @${m.participant.split`@`[0]} telah menghapus pesan!_*
+
+*[ • SGDC-BOT • ] _Detector_*
+`.trim(), m.message, {
+      contextInfo: {
+        mentionedJid: [m.participant]
+      }
+    })
+    this.copyNForward(m.key.remoteJid, m.message).catch(e => console.log(e, m))
+  }
+}
+
+module.exports = handler
