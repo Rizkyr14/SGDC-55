3a591317321081b153b8b287f2dce6c1bb2c647d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-04 09:54:22 +0800

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,21 +1,21 @@
 const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
 let handler  = async (m, { conn, args }) => {
-  m.reply('*[ WAIT ]* _Sedang Diproses..._\nJangan Spam BOT Jika Delay Nya Lama!')
+  m.reply('*[ WAIT ]* _Sedang Diproses..._\n```Jangan Spam Suu!```')
   let stiker = false
   try {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (/image|video/.test(mime)) {
       let img = await q.download()
-      if (!img) throw 'Tipe Tidak Diketahui!'
+      if (!img) throw '_Tipe Tidak Diketahui!_'
       stiker = await sticker(img, false, global.packname, global.author)
     } else if (args[0]) stiker = await sticker(false, args[0], global.packname, global.author)
   } finally {
     if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
-   else throw '*Kesalahan Pada Saat Mengonversi!*'
+   else throw '_Kesalahan Pada Saat Mengonversi!_'
   }
 }
 
