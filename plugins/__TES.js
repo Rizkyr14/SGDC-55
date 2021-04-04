12e4d0e7420491161f34361f2344d5e555d07385
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-04 21:49:53 +0800

    Add files via upload

---
+++ plugins/__TES.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+const FormData = require('form-data')
+
+let handler = async (m) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada foto'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  let img = await q.download()
+  let body = new FormData
+  body.append('image', img, 'image')
+  let res = await fetch('https://api.xteam.xyz/removebg?url=${mime}&APIKEY=abba3220ce4a347f', {
+    method: 'POST',
+    body
+  })
+  if (res.status !== 200) throw await res.json()
+  await conn.sendFile(m.chat, await res.buffer(), 'hd.jpg', 'Nih HD ga?\n\n*[ • SGDC-BOT • ]*', m)
+}
+
+handler.command = /^(tes)$/i
+handler.premium = true
+module.exports = handler
