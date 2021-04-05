eb70b9e69b09ad4d1427b16be516c0ff6a4647a3
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-05 22:12:26 +0800

    Update selfbot.js

--- plugins/selfbot.js
+++ plugins/selfbot.js
@@ -2,20 +2,20 @@ let { Presence, GroupSettingChange } = require('@adiwajshing/baileys')
 //  let isEnable = /true|enable|(turn)?on/i.test(command)
 let handler = async (m, { conn, usedPrefix, command, text, args, isROwner }) => {
    if (command == 'self') {
-		  isAll = true
-   if (!isROwner) {
-        global.dfail('rowner', m, conn)
-        throw false
-      }
-      global.opts['self'] = false
-  conn.reply(m.chat, `_Mode Self Berhasil Diaktifkan! Sekarang Hanya ROwner Yang Dapat Menggunakan BOT!_`, m)
-      } else if (command == 'public') {
-          isAll = false
+	 isAll = false
    if (!isROwner) {
         global.dfail('rowner', m, conn)
         throw false
       }
       global.opts['self'] = true
+  conn.reply(m.chat, `_Mode Self Berhasil Diaktifkan! Sekarang Hanya ROwner Yang Dapat Menggunakan BOT!_`, m)
+      } else if (command == 'public') {
+          isAll = true
+   if (!isROwner) {
+        global.dfail('rowner', m, conn)
+        throw false
+      }
+      global.opts['self'] = false
   conn.reply(m.chat, `_Mode Public Berhasil Diaktifkan! Sekarang Ssmua User Bisa Menggunakan BOT!_`, m)
       }
   }
