c500ec4c6c94fa10cbe870137316d477ea19ea14
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-05 22:17:25 +0800

    Update GhostCall.js

--- plugins/GhostCall.js
+++ plugins/GhostCall.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
 await axios.get(`https://api-anoncybfakeplayer.herokuapp.com/spamcall?nomor=${text}`).then ((res) => {
   let user = m.sender
  // let cal = `@${no.split("@")[0]}`
-  conn.reply(m.chat, `_Hey @${user.split("@")[0]}! Pastikan Nomor Yang Kamu Tuju Aktif Dan Bisa Dihubungi!, Karena Nomor Tersebut Akan Menerima Telpon Virtual Dari System Kami !!!_`, m, { contextInfo: { mentionedJid: user } })
+  conn.reply(m.chat, `_Hey @${user.split("@")[0]}! Pastikan Nomor Yang Kamu Tuju Aktif Dan Bisa Dihubungi!, Karena Nomor Tersebut Akan Menerima Telpon Virtual Dari System Kami !!!_`, m, { contextInfo: { mentionedJid: [user] } })
     })
 }
  
