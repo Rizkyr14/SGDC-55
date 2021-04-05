5a1c70d9d69de3d7ec73ae9ceb4a79c6d08502de
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-05 22:01:30 +0800

    Update DoaTahlil.js

--- plugins/DoaTahlil.js
+++ plugins/DoaTahlil.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, text }) => {
                 let doa = res.data.result
                 let out = `*「  DO'A TAHLIL  」*\n\n`
                 for (let i = 0; i < doa.data.length; i++) {
-                    out += `─────────────────────\n*${doa.data[i].title}*\n${doa.data[i].arabic}\n\n*Artinya*\n${doa.data[i]translation}\n`
+                    out += `─────────────────────\n*${doa.data[i].title}*\n${doa.data[i].arabic}\n\n*Artinya*\n${doa.data[i].translation}\n`
                 }
                     out += '\n*[ • SGDC-BOT • ]*'
                  conn.reply(m.chat, out, m)
