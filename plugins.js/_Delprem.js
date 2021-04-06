278a4a6116456ba8a0f9264cf23de2029aabf00e
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-06 23:04:11 +0700

    Update _Delprem.js

--- plugins.js/_Delprem.js
+++ plugins.js/_Delprem.js
@@ -1,5 +1,4 @@
-let axios = require("axios");
-const { MessageType } = require('@adiwajshing/baileys')
+ const { MessageType } = require('@adiwajshing/baileys')
 const fs = require('fs')
 let handler = async(m, { conn, text, participants, isPrems }) => {
 let who
@@ -7,18 +6,17 @@ let who
   else who = m.chat
   if (!who) throw '_Tag orang yang akan dijadikan user premium!_'
   let user = `${who.split("@s.whatsapp.net")[0]}`
-  let up = global.prems.splice(user)
+  let up = global.prems.splice(user, 1)
   fs.writeFileSync('./config.js',JSON.stringify(up))
   let prem = `
 *╭═══[ • SGDC-BOT • ]═══╮*
+
 _Berhasil Add User Premium_
 _~> Nomor: wa.me/${who.split("@s.whatsapp.net")[0]}_
 `.trim()
   conn.reply(m.chat, prem, m)
 }
-
 handler.command = /^(delprem)$/i
+handler.rowner = true
 
 module.exports = handler
-
-//  MUHAMMAD AFDHAN
