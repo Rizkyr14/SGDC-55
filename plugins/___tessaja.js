a9913c6e68d43f631ae0a8318b0a8dc4f461b1a3
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-06 11:34:21 +0800

    Update ___tessaja.js

--- plugins/___tessaja.js
+++ plugins/___tessaja.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, args }) => {
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.quoted
   let usname = conn.getName(who)
-  let mentionedJid = [m.sender]
+  let mentionedJid = [m.quoted]
   let name = m.fromMe ? conn.user : conn.contacts[m.sender]
   let users = m.quoted
    conn.groupRemove(m.chat, [users])
