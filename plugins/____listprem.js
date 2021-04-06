da9c635baa9ecf0606398e843599ac8287a68226
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-07 00:32:33 +0700

    Add files via upload

---
+++ plugins/____listprem.js
@@ -0,0 +1,34 @@
+const { MessageType } = require('@adiwajshing/baileys')
+const fs = require('fs')
+let handler = async(m, { conn, text, participants, isPrems }) => {
+
+let global.prems = JSON.parse(JSON.stringify(fs.readFileSync('./config.js')))
+     let kntl = global.prems
+     let user = `Total User Premium: ${kntl.length}\n`
+     for (let kon of kntl) {
+         user += `➸ @${kon.split("@")[0]}\n`
+        }
+        user += '\n*[ • SGDC-BOT • ]*'
+
+  conn.reply(m.chat, user, m, {
+           contextInfo: { 
+                 mentionedJid: [kon]
+          } 
+     })
+  }
+  
+handler.command = /^(listprem)$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+//  MUHAMMAD AFDHAN
+
+module.exports = handler
