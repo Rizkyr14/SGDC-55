7d8114dc30f6ff966e95f5e6c9b6cb0b5bbba684
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-06 21:02:40 +0700

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,10 +1,16 @@
-let handler = (m, { text }) => {
+let handler = async(m, { conn, text }) => {
   let user = global.DATABASE.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`
-_${conn.getName(m.sender)} Sekarang AFK!_\nKarena${text ? ': ' + text : ''}
-`)
+  let user = m.sender
+  let aef = `
+_@${user.split("@")[0]} Sekarang AFK!_\nKarena${text ? ': ' + text : ''}
+`.trim()
+conn.reply(m.chat, aef, m, {
+           contextInfo: { 
+                 mentionedJid: [user] 
+          } 
+     })
 }
 
 handler.command = /^afk$/i
