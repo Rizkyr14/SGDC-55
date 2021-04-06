b209c6dd86b9d973f2d0f5965410c522a9ab50ae
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-06 11:30:54 +0800

    Update ___tessaja.js

--- plugins/___tessaja.js
+++ plugins/___tessaja.js
@@ -1,6 +1,10 @@
 let handler = async (m, { conn, args }) => {
-   let user = { message: { [m.quoted.mtype]: m.quoted }}
-   await conn.groupRemove(m.chat, [user])
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let usname = conn.getName(who)
+  let mentionedJid = [m.sender]
+  let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+  let users = m.quoted
+   conn.groupRemove(m.chat, [users])
 }
 handler.customPrefix = /^kill $/i
 handler.command = new RegExp
