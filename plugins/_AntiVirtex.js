d1a3376aa6d6d2a097a10fe2de85904fdfb9fd22
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-07 07:05:58 +0700

    Add files via upload

---
+++ plugins/_AntiVirtex.js
@@ -0,0 +1,28 @@
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async function(m, { conn , args, text, isAdmin, isBotAdmin }) {
+
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let usname = conn.getName(who)
+  let mentionedJid = [m.sender]
+  let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+  let users = m.sender
+  let vir = users.split("@s.whatsapp.net")[0]
+  if (text.length > 5) {
+  	
+    await conn.reply(m.chat, `
+*[ CHAT ANTI VIRTEX ]*
+
+_Terdeteksi *${usname}* telah mengirim link group!_
+
+Maaf Kamu akan dikick oleh *SGDC-BOT*
+`.trim(), m)
+ //conn.groupRemove(m.chat, [users])
+     conn.sendMessage(vir + '@s.whatsapp.net', ${text}, MessageType.text)
+       }
+}
+handler.customPrefix = /^-?[a-z]+(\.[A-Z]+)?$/
+handler.command = new RegExp
+
+handler.fail = null
+
+module.exports = handler
