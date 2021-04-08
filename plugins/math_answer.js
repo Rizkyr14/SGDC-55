6133241edfce5612b6b4586f5d9dc2f4402ae6af
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-08 21:19:47 +0800

    Oo

--- plugins/math_answer.js
+++ plugins/math_answer.js
@@ -2,27 +2,25 @@ let handler = async (m, { conn }) => {
   let id = m.chat
   if (!m.quoted || m.quoted.sender != conn.user.jid || !/^Berapa hasil dari/i.test(m.quoted.text)) throw false
   conn.math = conn.math ? conn.math : {}
-  if (!(id in conn.math)) throw '_Lu telat ngen' + readMore + '(uhh) :v_'
+  if (!(id in conn.math)) throw 'Ente Dah Telat :v'
   if (m.quoted.id == conn.math[id][0].id) {
   let math = JSON.parse(JSON.stringify(conn.math[id][1]))
   if (m.text == math.result) {
     global.DATABASE._data.users[m.sender].exp += math.bonus
     clearTimeout(conn.math[id][3])
     delete conn.math[id]
-    throw `*Jawaban Benar!* _Selamat A${readMore}nda Tidak Mendapatkan Apa-Apa :D`
+    throw `*Jawaban Benar!*`
   } else {
     if (--conn.math[id][2] == 0) {
       clearTimeout(conn.math[id][3])
       delete conn.math[id]
-      throw `*Kesempatan Habis!* _Jawabannya *${math.result}*_`
-    } else throw `*Jawaban Salah!* _Masih Ada *${conn.math[id][2]}* Kesempatan_`
+      throw `*Kesempatan habis!* _Jawaban: *${math.result}*_`
+    } else throw `*Jawaban Salah!* _Masih ada ${conn.math[id][2]} kesempatan_`
   }
  }
 }
 handler.customPrefix = /^-?[0-9]+(\.[0-9]+)?$/
 handler.command = new RegExp
-
-
 module.exports = handler
 
 
