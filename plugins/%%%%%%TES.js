a14001bcbe49985c88f2054320b6b7e42a5b145c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-09 13:47:38 +0800

    Update %%%%%%TES.js

--- plugins/%%%%%%TES.js
+++ plugins/%%%%%%TES.js
@@ -1,38 +1,23 @@
-const fetch = require('node-fetch')
-const FormData = require('form-data')
 const { MessageType } = require('@adiwajshing/baileys')
-const imageToBase64 = require('image-to-base64');
-const axios = require("axios");
-let handler  = async (m, { conn, text }) => {
-	if (!text) return m.reply('_Masukkan Teks_')
-	if (text.length > 15) return m.reply('_Teks Kepanjangan!_')
-//let api = 'abba3220ce4a347f'
-	  await m.reply('*[ WAIT ]* _Sedang Diproses..._')
-	 await axios.get('https://api.xteam.xyz/sticker/stickerly?q=' + text + '&APIKEY=abba3220ce4a347f')
-	.then((res) => {
-	let bau = res.data.result
-        let b = JSON.parse(JSON.stringify(bau.stickerlist));
-        let mmk =  b[Math.floor(Math.random() * b.length)];
-    /*let img = await mmk.buffer()
-    if (!img) throw img
-    let stiker = await sticker(img)*/
-        imageToBase64(mmk)
-        .then(
-            (response) => {
-	let buf = Buffer.from(response, 'base64');
-        //let knt = encodeURIComponent(buf)
-    conn.sendMessage(m.chat, buf, 'ss.png', MessageType.image, { quoted: m })
-             }
-        )
-        .catch(
-            (error) => {
-                console.log(error); 
-            }
-        )
-    
-    });
-}	      
-handler.command = /^(tod)$/i
+
+let handler = async(m, { conn, text }) => {
+    if (!text) return conn.reply(m.chat, '_Masukkan Text Laporan!_', m)
+    if (text > 200) return conn.reply(m.chat, '_Teks Terlalu Panjang!, Maksimal 200 Karakter_', m)
+    /*let name = conn.getName(m.sender)
+    var nomor = m.sender
+    const reprot = `
+*╭══════[ REPORT ]══════╮*
+~ • Nama: ${name}
+~ • Nomor: wa.me/${nomor.split("@s.whatsapp.net")[0]}
+~ • Laporan: ${text}
+*╰═════════════════╯*
+`.trim()*/
+    conn.sendMessage('0@s.whatsapp.net', `${text}`, MessageType.text)
+   // conn.reply(m.chat, '_Masalah telah di laporkan ke Owner *SGDC-BOT*_', m)
+}               
+
+
+handler.command = /^(mark)$/i
 
 handler.botAdmin = false
 
