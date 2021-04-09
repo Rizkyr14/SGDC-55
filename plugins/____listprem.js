30984175e86083b76fed723458a9a816c073886f
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-09 14:13:41 +0800

    Update ____listprem.js

--- plugins/____listprem.js
+++ plugins/____listprem.js
@@ -1,10 +1,11 @@
 let handler = async (m, { conn, usedPrefix }) => {
     //let name = m.fromMe ? conn.user : conn.contacts[m.sender]
     let nama = conn.getName(m.sender)
+    let ayam = m.sender
     let own = '6283159600193@s.whatsapp.net'
-  conn.reply(m.chat, `_Hai Kak ${nama}, Ketik *${usedPrefix}menu* Untuk Memulai @${own.split("@")[0]}_`, m, { 
+  conn.reply(m.chat, `_Hai Kak @${ayam.split("@")[0]}, Ketik *!menu* Untuk Memulai @${own.split("@")[0]}_`, m, { 
       contextInfo: { 
-          mentionedJid: [own] 
+          mentionedJid: [ayam, own] 
       } 
   })
     //let mentionedJid = [m.sender]
