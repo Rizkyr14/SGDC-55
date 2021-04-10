2505880a87f3c450a4bfdd9ca0f895a33092fc21
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-10 18:38:26 +0800

    Add files via upload

---
+++ plugins/SendKontak.js
@@ -0,0 +1,18 @@
+let { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn, text }) => {
+	let [mmek, kntl] = args.join` `
+    if (!mmek) return m.reply('_Tag Salah Satu Stah!_')
+	if (!kntl) return m.reply('_Dia Gapunya Nama Ya??_')
+	
+	let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+	var no = m.mentionedJid[0]
+	let nom = no.split("@s.whatsapp.net")[0]
+
+  conn.sendMessage(m.chat, `${nom}`, `${kntl}`, MessageType.contact, { contextInfo: { mentionedJid: users } })
+}
+handler.command = /^(sendkontak|kontak)$/i
+
+handler.fail = null
+
+module.exports = handler
