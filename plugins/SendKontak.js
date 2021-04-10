db852becab6ccd4db6bcd2ae2bc42cfb9e36157d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-10 19:03:33 +0800

    Update SendKontak.js

--- plugins/SendKontak.js
+++ plugins/SendKontak.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 if (args.startsWith(global.prefix)) {
 var user = m.sender
-         conn.reply(m..chat, `Maaf Kak @${user.split ("@")[0]}, Command *${usedPrefix}${command}* Tidak Ada Di Dalam *${usedPrefix}menu*`, m, { 
+         conn.reply(m.chat, `Maaf Kak @${user.split ("@")[0]}, Command *${usedPrefix}${command}* Tidak Ada Di Dalam *${usedPrefix}menu*`, m, { 
                   contextInfo: { 
                            mentionedJid: [user] 
                   }
