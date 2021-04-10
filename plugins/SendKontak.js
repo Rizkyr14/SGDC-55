f6feb98a711007d3bdb822f925bc82d4b439f2b4
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-10 16:09:23 +0800

    Update SendKontak.js

--- plugins/SendKontak.js
+++ plugins/SendKontak.js
@@ -1,4 +1,4 @@
-let handler = function (m, { conn, text })  {
+let handler = async (m, {conn, text }) => {
 //let [mmek, kntl] = text.split ` `
 //        if (!mmek) return m.reply('_Tag Salah Satu Stah!_')
 
@@ -9,9 +9,13 @@ let handler = function (m, { conn, text })  {
        // let name = conn.getName(m.mentionedJid)
 	let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
 	var no = m.mentionedJid[0]
-	let nom = no.split("@s.whatsapp.net")[0]
+	let nom = `${no.split("@s.whatsapp.net")[0]}`
 
-  this.sendContact(m.chat, '${nom}', `${text}`, m, { contextInfo: { mentionedJid: [users] } })
+  conn.sendContact(m.chat, nom, `${text}`, m, { 
+	  contextInfo: { 
+		  mentionedJid: [users] 
+	  } 
+    })
 }
 handler.command = /^(sendkontak|kontak)$/i
 
