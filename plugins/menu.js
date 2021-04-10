3c335bc484ab4c7e4aae825fd66f92a8d2a60948
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-10 16:53:56 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -12,7 +12,7 @@ let handler  = async (m, { conn, args, usedPrefix: _p }) => {
     let bname = package.name
     let vers = package.version
     let dsci = package.description
-    let res = await axios.get(`https://xinzbot-api.herokuapp.com/api/hitungmundur?apikey=XinzBot&tanggal=12&bulan=4`)
+    let res = await axios.get(`https://xinzbot-api.herokuapp.com/api/hitungmundur?apikey=XinzBot&tanggal=13&bulan=4`)
     let rmd = res.data.result
     let gc = 'https://tinyurl.com/ygu7vxny'
     let name = conn.getName(m.sender)
