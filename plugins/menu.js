5256302788f3787d6947d525e43aa49e22d43c2d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-12 01:41:56 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -385,7 +385,7 @@ ${readMore}
   conn.fakeReply(m.chat, 'Untuk Menu Gretongan, Ketik *!gretongmenu*', '0@s.whatsapp.net', '*MENU GRETONGAN SGDC-TEAM*')
   } catch (e) {
     conn.fakeReply(m.chat, '*TERJADI KESALAHAN PADA SAAT MEMUAT MENU!*', '0@s.whatsapp.net', '*MENU ERROR! SEGERA LAPORKAN KE OWNER!*')
-    //throw e
+    throw e
    // conn.sendMessage('6282252655313@s.whatsapp.net', 'Menu Error\n' + util.format(e), MessageType.text)
   }
 }
