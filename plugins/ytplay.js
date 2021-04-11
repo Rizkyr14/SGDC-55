9d84217122a18f482d22802b26f7e1539786e2be
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-11 10:04:50 +0800

    Update ytplay.js

--- plugins/ytplay.js
+++ plugins/ytplay.js
@@ -20,8 +20,10 @@ let handler = async(m, { conn, text }) => {
 *Channel:* ${res.data.result.channel}
 *Published:* ${res.data.result.published_on}
 *Views:* ${res.data.result.views}
+*Source:* ${res.data.result.source}
 *Url:* ${res.data.result.url}
-*Description:* ${res.data.result.description}
+*Description:*
+${res.data.result.description}
 
 _Download Sendiri, Jangan Manja :v_
 
