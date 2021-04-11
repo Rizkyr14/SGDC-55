52c9274ebf7b6b67879d13b428de6e542d139b0b
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-11 16:43:46 +0800

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -5,7 +5,7 @@ let chat = global.DATABASE.data.chats[m.chat]
 if (chat.simi) {
         let res = await fetch(`https://videfikri.com/api/simsimi/?teks=${text}`)
         let json = await res.json()
-  if (json.result) m.reply(json.result.jawaban)
+  if (json.result) conn.reply(m.chat, json.result.jawaban, m)
   else throw 'ERROR'
   } else throw '*SIMI BELUM DIAKTIFKAN UNTUK CHAT INI!*'
 }
