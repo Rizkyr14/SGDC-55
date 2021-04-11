11f7d088933fa48a3c96fa8f35ad1173fae62008
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-11 16:13:03 +0800

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
 let chat = global.DATABASE.data.chats[m.chat]
 if (chat.simi) {
-	let res = await fetch('https://simsumi.herokuapp.com/api?text=${text}&lang=id')
+	let res = await fetch(`https://simsumi.herokuapp.com/api?text=${text}&lang=id`)
 	let json = await res.json()
 	if (json.success) m.reply(json.success)
     else throw 'Ngomong Apa?'
