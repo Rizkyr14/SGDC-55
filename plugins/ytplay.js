ca87026406f493057cb20984e60197766758b110
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-11 09:51:34 +0800

    Update ytplay.js

--- plugins/ytplay.js
+++ plugins/ytplay.js
@@ -4,19 +4,24 @@ let handler = async(m, { conn, text }) => {
  if (!text) return conn.reply(m.chat, '_Masukkan Judul Video_', m)
   await m.reply('*[ WAIT ]* _Sedang Diproses.._')
 
-    axios.get(`https://fzn-gaz.herokuapp.com/api/ytplay?judul=${text}`)
+    axios.get(`https://videfikri.com/api/ytplay/?query=${text}`)
     .then((res) => {
-      imageToBase64(res.data.image)
+      imageToBase64(res.data.result.thumbnail)
         .then(
           (ress) => {
             let buf = Buffer.from(ress, 'base64')
             let str = `
 *KLIK LINK FOR DOWNLOAD*               
             
-*Title:* ${res.data.title}
-*Size:* ${res.data.size}
-*Link:* ${res.data.result}
 
+*Title:* ${res.data.result.title}
+*Size:* ${res.data.result.size}
+*Durasi:* ${res.data.result.duration}
+*Channel:* ${res.data.result.channel}
+*Published:* ${res.data.result.published_on}
+*Views:* ${res.data.result.views}
+*Url:* ${res.data.result.url}
+*Description:* ${res.data.result.description}
 
 _Download Sendiri, Jangan Manja :v_
 
