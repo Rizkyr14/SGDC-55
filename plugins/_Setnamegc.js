6d0a25ebc6d513eb952f373680980d2c988f4523
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-12 13:58:30 +0800

    Update _Setnamegc.js

--- plugins/_Setnamegc.js
+++ plugins/_Setnamegc.js
@@ -1,9 +1,17 @@
 let handler = async (m, { conn, args, text, isBotAdmin, isAdmin }) => {
 	
-                if (!text) return m.reply('Masukkan Teks')
-			    if (!isAdmin) return m.reply('_Anda Siapa?_')
-				if (!isBotAdmin) return m.reply('_SGDC-BOT Harus Jadi Admin!_')
-                await conn.groupUpdateSubject(m.chat, `${text}`)
-                conn.reply(m.chat, '_Berhasil Mengganti Nama Grup!_', m)
+        if (!text) return m.reply('Masukkan Teks')
+	if (!isAdmin) return m.reply('_Anda Siapa?_')
+	if (!isBotAdmin) return m.reply('_SGDC-BOT Harus Jadi Admin!_')
+        await conn.groupUpdateSubject(m.chat, `${text}`)
+        conn.reply(m.chat, '_Berhasil Mengganti Nama Grup!_', m)
             
 }
+
+handler.command = /^(setname(gc)?)$/i
+
+handler.group = true
+
+handler.fail = null
+module.exports = handler
+
