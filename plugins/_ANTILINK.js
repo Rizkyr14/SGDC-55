6f2a29ec163adb91f91d043e3402016f46dafde5
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-12 20:42:39 +0800

    Update _ANTILINK.js

--- plugins/_ANTILINK.js
+++ plugins/_ANTILINK.js
@@ -4,30 +4,32 @@ let handler = async function(m, { conn , args, isAdmin, isBotAdmin }) {
   let usname = conn.getName(who)
   let mentionedJid = [m.sender]
   let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-  let users = m.sender
+  let ajg = m.sender
+  let bott = 'SGDC-BOT'
   let chat = global.DATABASE.data.chats[m.chat]
+ if (chat.antiLink) {
+ if (!m.isGroup) return
  if (isAdmin) return m.reply('_Kali ini anda aman! Karena anda adalah Admin !!!_')
- if (chat.antiLink) { 
  await conn.reply(m.chat, `
 *[ CHAT ANTI LINK ]*
 
-_Terdeteksi *@${users.split("@")[0]}* telah mengirim link group!_
+_Terdeteksi *@${ajg.split("@")[0]}* telah mengirim link group!_
 
-Maaf Kamu akan dikick oleh *SGDC-BOT*
+Maaf Kamu akan dikick oleh *@${bott.split("@")[0]}*
 `.trim(), m, { 
    contextInfo: { 
-     mentionedJid: [users]
+     mentionedJid: [ajg, bott]
    } 
  })
  if(isBotAdmin) { 
-   conn.groupRemove(m.chat, [users])
+   conn.groupRemove(m.chat, [ajg])
     } else if (!isBotAdmin) return m.reply('_Jadikan SGDC-BOT Sebagai Admin, Agar Bisa Bertindak !!!_')
-    } else if (!chat.antiLink) return m.reply('```AKTIFIN ANTILINK! BIAR GUA KILL NI BOCAH !!```')
+    } else if (!chat.antiLink) return m.reply('```AKTIFIN ANTILINK! BIAR GUA ULTI NI BOCAH !!!```')
  }
 
 handler.customPrefix = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 handler.command = new RegExp
-handler.group = true
+
 handler.fail = null
 
 module.exports = handler
