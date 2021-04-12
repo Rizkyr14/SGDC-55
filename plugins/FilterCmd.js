ab68151e9ca72f3c0b4efaa693e1a9372d74cf76
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-12 16:16:06 +0800

    Update FilterCmd.js

--- plugins/FilterCmd.js
+++ plugins/FilterCmd.js
@@ -1,7 +1,7 @@
 let handler = (m, { conn, usedPrefix, command }) => {
 let name = conn.getName(m.sender)
 let peler = m.sender
- conn.reply(m.chat, `_Maaf @${peler.split("@")[0]}! Command *${m.text}* Tidak Terdaftar Di *${usedPrefix}menu!*_`, m, { 
+ conn.reply(m.chat, `_Maaf Kak @${peler.split("@")[0]}! Command *${m.text}* Tidak Terdaftar Di *${usedPrefix}menu*_`, m, { 
   contextInfo: { 
    mentionedJid: [peler]
   }
