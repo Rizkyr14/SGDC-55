caca1333e43cbbadfafe7711640a1092ea7d347b
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-12 13:54:05 +0800

    Add files via upload

---
+++ plugins/_Setnamegc.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, args, text, isBotAdmin, isAdmin }) => {
+	
+                if (!text) return m.reply('Masukkan Teks')
+			    if (!isAdmin) return m.reply('_Anda Siapa?_')
+				if (!isBotAdmin) return m.reply('_SGDC-BOT Harus Jadi Admin!_')
+                await conn.groupUpdateSubject(m.chat, `${text}`)
+                conn.reply(m.chat, '_Berhasil Mengganti Nama Grup!_', m)
+            
+}
