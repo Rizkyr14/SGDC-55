d46890a5053f7cc7b5bbbb2a4d2fe7e486c0d1b0
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-12 16:29:14 +0800

    Update reprot.js

--- plugins/reprot.js
+++ plugins/reprot.js
@@ -2,14 +2,15 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, '_Masukkan Text Laporan!_', m)
-    if (text > 200) return conn.reply(m.chat, '_Teks Terlalu Panjang!, Maksimal 200 Karakter_', m)
+    if (text.length > 200) return conn.reply(m.chat, '_Teks Terlalu Panjang!, Maksimal 200 Karakter_', m)
     let name = conn.getName(m.sender)
     var nomor = m.sender
     const reprot = `
 *╭══════[ REPORT ]══════╮*
 ~ • Nama: ${name}
 ~ • Nomor: wa.me/${nomor.split("@s.whatsapp.net")[0]}
-~ • Laporan: ${text}
+~ • Laporan:
+${text}
 *╰═════════════════╯*
 `.trim()
     conn.sendMessage('6282252655313@s.whatsapp.net', reprot, MessageType.text)
