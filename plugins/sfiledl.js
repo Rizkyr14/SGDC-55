fb952aaf6176b99bcb9d542314d6a5d40cbeaafe
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-12 15:11:20 +0800

    Update sfiledl.js

--- plugins/sfiledl.js
+++ plugins/sfiledl.js
@@ -19,7 +19,7 @@ _File Nya Langsung Dikirim_
 
 *[ • SGDC-BOT • ]*
 `.trim())
-  if (res.data.size > 02.00) {
+  if (res.data.size > '02.00 MB') {
 	  m.reply('_File Anda Terlalu Besar! Silahkan Download Sendiri Melalui Link Yang Dikirim SGDC-BOT!_')
 	  } else {
    conn.sendFile(m.chat, res.data.result, `${res.data.title}`, m)
