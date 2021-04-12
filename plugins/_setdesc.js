89253c3cb4076a92754986797df4440d43d2d8b3
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-12 13:42:30 +0800

    Update _setdesc.js

--- plugins/_setdesc.js
+++ plugins/_setdesc.js
@@ -2,17 +2,12 @@ let handler = async (m, { conn, args, text, isBotAdmin, isAdmin }) => {
 
         if (!isAdmin) return m.reply('_Anda Siapa?_')
 	if (!isBotAdmin) return m.reply('_SGDC-BOT Harus Jadi Admin!_')
-        conn.groupUpdateDescription(m.chat, `${text}`)
+        await conn.groupUpdateDescription(m.chat, `${text}`)
         conn.reply(m.chat, '_Berhasil Mengganti Deskripsi Grup!_', m)
  }            
 handler.command = /^(setdesc)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = true
-handler.private = false
 
-handler.admin = true
+handler.group = false
 handler.botAdmin = true
 
 handler.fail = null
