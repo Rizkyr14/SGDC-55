ad80da4298585393450e5e39a4078e1387dfa9d5
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-14 00:49:30 +0800

    Update ____Tesimgbb.js

--- plugins/____Tesimgbb.js
+++ plugins/____Tesimgbb.js
@@ -1,4 +1,4 @@
-let imgbb = require("imgbb-uploader");
+let imgBB = require("imgbb-uploader");
 let ffmpeg = require('fluent-ffmpeg')
 let { MessageType } = require('@adiwajshing/baileys')
 let { exec } = require('child_process')
@@ -13,16 +13,16 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
     const isQuotedImage = type === 'extendedTextMessage' && content.includes('imageMessage')
     const isQuotedVideo = type === 'extendedTextMessage' && content.includes('videoMessage')
 
-   // var imgbb = require('imgbb-uploader')
+    var imgBB = require('imgbb-uploader')
     if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
       let ngntd = isQuotedImage ? JSON.parse(JSON.stringify(m).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : m
       let media = await conn.downloadAndSaveMediaMessage(ngntd)
-   //tels = body.slice(7)
-      let anu = await imgbb("08579d070df9a07cb1c2ee565aece767", media)
-      //let res = 'https://videfikri.com/api/textmaker/customwp/?urlgbr=' + anu.display_url
-      let res = await axios.get(`https://videfikri.com/api/textmaker/customwp/?urlgbr=${anu.display_url}`)
-      let hehe = res.data
-        conn.reply(m.chat, hehe, 'TES.png', 'Nih Stah', m)
+      //let tels = body.slice(7)
+      let anu = await imgBB("08579d070df9a07cb1c2ee565aece767", media)
+      let res = 'https://videfikri.com/api/textmaker/customwp/?urlgbr=' + anu.display_url;
+      /*let res = await axios.get(`https://videfikri.com/api/textmaker/customwp/?urlgbr=${anu.display_url}`)
+      let hehe = res.data*/
+        conn.reply(m.chat, res, 'TES.png', 'Nih Stah', m)
         } else {
         m.reply('Reply!')
        }
