ab0def5ea3eaa5d77578e7d1f6f8619a97fd1304
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-14 01:27:12 +0800

    Update ____Tesimgbb.js

--- plugins/____Tesimgbb.js
+++ plugins/____Tesimgbb.js
@@ -22,7 +22,7 @@ let handler = async(m, { conn, text, args, bot, command }) => {
      // let res = 'https://videfikri.com/api/textmaker/customwp/?urlgbr=' + anu.display_url;
       let res = await axios.get(`https://videfikri.com/api/textmaker/customwp/?urlgbr=${anu.display_url}`)
       let hehe = res.data
-        conn.reply(m.chat, res, 'TES.png', 'Nih Stah', m)
+        conn.reply(m.chat, hehe, 'TES.png', 'Nih Stah', m)
         } else {
         m.reply('Reply!')
        }
