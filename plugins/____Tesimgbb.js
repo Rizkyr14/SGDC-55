02161fc1a29b2d6324647776e4ca3505aca6e7d1
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-14 01:36:30 +0800

    Update ____Tesimgbb.js

--- plugins/____Tesimgbb.js
+++ plugins/____Tesimgbb.js
@@ -19,10 +19,10 @@ let handler = async(m, { conn, text, args, bot, command }) => {
       let media = await conn.downloadAndSaveMediaMessage(ngntd)
       let tels = m.text
       let anu = await imgBB("3ea1465ef91578a90ee81f7d41c59a1f", media)
-     // let res = 'https://videfikri.com/api/textmaker/customwp/?urlgbr=' + anu.display_url;
-      let buff = await axios.get(`https://videfikri.com/api/textmaker/customwp/?urlgbr=${anu.display_url}`)
+      let res = 'https://videfikri.com/api/textmaker/customwp/?urlgbr=' + anu.display_url;
+     // let buff = await axios.get(`https://videfikri.com/api/textmaker/customwp/?urlgbr=${anu.display_url}`)
       //let hehe = res.data
-        conn.sendFile(m.chat, buff, 'TES.png', 'Nih Stah', m)                   
+        conn.sendFile(m.chat, res, 'TES.png', 'Nih Stah', m)                   
         } else {
         m.reply('Reply!')
        }
