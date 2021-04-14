3b3593bf4cecf21e1655ff149fa441b39fdfc49c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-14 17:25:13 +0700

    Update ____Tesimgbb.js

--- plugins/____Tesimgbb.js
+++ plugins/____Tesimgbb.js
@@ -2,6 +2,8 @@ let imgBB = require("imgbb-uploader");
 let { MessageType } = require('@adiwajshing/baileys');
 let { spawn } = require('child_process');
 let path = require('path');
+const { sticker } = require('../lib/sticker');
+let fetch = require('node-fetch');
 let util = require('util');
 let fs = require('fs');
 let axios = require("axios");
@@ -19,9 +21,17 @@ let handler = async(m, { conn, text, args, bot, command }) => {
         media = await conn.downloadAndSaveMediaMessage(ngntd)
         anu = await imgBB("3ea1465ef91578a90ee81f7d41c59a1f", media)
         res = 'https://videfikri.com/api/textmaker/customwp/?urlgbr=' + anu.display_url;
+     let triger = await fetch('https://some-random-api.ml/canvas/triggered?avatar=' + encodeURIComponent(anu.display_url));
+     let img = await triger.buffer()
+     let stiker = await sticker(img, false, global.packname, global.author)
+     //   stic = await sticker(false, triger, global.packname, global.author)
+       
      // let buff = await axios.get(`https://videfikri.com/api/textmaker/customwp/?urlgbr=${anu.display_url}`)
       //let hehe = res.data
         conn.sendFile(m.chat, res, 'TES.jpg', 'Nih Stah', m)                   
+        conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
         } else {
         m.reply('Reply Fotonya!')
         }
