5d25383aa59eb8b135e78f2baa9d8b0f7139acf9
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-14 13:28:31 +0800

    Update _Attp2.js

--- plugins/_Attp2.js
+++ plugins/_Attp2.js
@@ -1,10 +1,11 @@
 const fetch = require('node-fetch')
 const FormData = require('form-data')
 const { MessageType } = require('@adiwajshing/baileys')
-
+const kntl = require("../src/kntl.json");
 let handler  = async (m, { conn, text }) => {
   await m.reply('*[ WAIT ]* _Sedang membuat..._')
-  if (text) conn.sendFile(m.chat, 'http://lolhuman.herokuapp.com/api/attp?apikey=761e676c13e7710a48011b2b&text=' + encodeURIComponent(text), 'attp.webp', '', m, false, { asSticker: true })
+  let api = (kntl.apikey)
+  if (text) conn.sendFile(m.chat, 'http://lolhuman.herokuapp.com/api/attp?apikey=' + api + '&text=' + encodeURIComponent(text), 'attp.webp', '', m, false, { asSticker: true })
   else throw 'Teksnya mana mbah?'
 }
 
