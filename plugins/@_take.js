25a1594a1dcf4992158cd1b8b79d014eb33ef2a4
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-14 19:43:36 +0700

    Add files via upload

---
+++ plugins/@_take.js
@@ -0,0 +1,24 @@
+const { MessageType } = require('@adiwajshing/baileys')
+const { sticker } = require('../lib/sticker')
+let handler  = async (m, { conn, args, text }) => {
+  let [kontol, memek] = text.split `|`
+  let stiker = false
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (/image|video/.test(mime)) {
+      let img = await q.download()
+      if (!img) throw 'Foto/Video tidak ditemukan'
+      stiker = await sticker(img, false, kontol, memek)
+    } else if (args[0]) stiker = await sticker(false, args[0], kontol, memek)
+  } finally {
+    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+    else throw 'Conversion failed'
+  }
+}
+
+handler.command = /^stikerwm$/i
+
+module.exports = handler
