f142738f7a6c67c03c256aa7021f20190b52c818
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-20 14:56:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
-console.log('Connecting SGDC-BOT...')
+console.log('Starting SGDC-BOT...')
 let { spawn } = require('child_process')
 let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say(`${package.name}@^${package.version}\n${package.description}`, {
+CFonts.say('=============================================', {
   font: 'console',
   align: 'center',
-  gradient: ['red', 'green']
+  colors: ['red'],
+  gradient: false
 })
-CFonts.say('MUHAMMAD AFDHAN', {
+CFonts.say(`${package.name}`, {
+  font: 'pallet',
+  align: 'center',
+  colors: ['whiteBright','red'],
+  background: 'transparent',
+  letterSpacing: 0,
+  lineHeight: 1,
+  space: false,
+  maxLength: '0', 
+  gradient: false,
+  independentGradient: false,
+  transitionGradient: false,
+  env: 'node'
+})
+CFonts.say('=============================================', {
   font: 'console',
   align: 'center',
-  gradient: ['red', 'magenta']
+  colors: ['red'],
+  gradient: false
 })
-
+CFonts.say(`Tetaplah menjadi dirimu sendiri|jangan pernah mencoba menjadi orang lain|Ingat! satu rasa bukan berarti satu raga.\n\nPluto, 20 April 2021`, {
+  font: 'console',
+  align: 'center',
+  colors: ['cyan'],
+  letterSpacing: 0,
+  space: false,
+  gradient: true,
+})
+CFonts.say(`${package.name}@${package.version}\n\nMUHAMMAD AFDHAN`, {
+  font: 'console',
+  align: 'center',
+  colors: ['red']
+})
 function start(file) {
   let args = [path.join(__dirname, file), ...process.argv.slice(2)]
   CFonts.say([process.argv[0], ...args].join(' '), {
     font: 'console',
     align: 'center',
-    gradient: ['red', 'magenta']
+    colors: ['gray'],
+    gradient: false
   })
   let p = spawn(process.argv[0], args, {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc']
   })
   p.on('message', data => {
-    console.log('[ SGDC-BOT RECEIVED DATA ]', data)
+    console.log('SGDC-BOT [ RECEIVED ] =>', data)
     switch (data) {
       case 'reset':
         p.kill()
@@ -47,5 +76,3 @@ function start(file) {
   })
 
 }
-
-start('dhans.js')
