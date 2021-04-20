0ea1ef5ce971e63d0c4257b2a0030661d381c91b
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-20 16:14:43 +0800

    Update randomfeet.js

--- plugins/randomfeet.js
+++ plugins/randomfeet.js
@@ -8,7 +8,7 @@ let chat = global.DATABASE.data.chats[m.chat]
       let link = 'http://lolhuman.herokuapp.com/api/random2/feet?apikey=' + api;
       conn.sendFile(m.chat, link, 'SGDC-BOT.png', 'No Colay:v\n\n*SGDC-BOT*', m)
    } else {
-        m.reply('```Perlu Mengaktifkan Mode NSFW```')
+       m.reply('```Perlu Mengaktifkan Mode NSFW```')
    }
  } 
 
